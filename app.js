10bdc2ebf5788012084c938b67e3e32d1e1be802
RominaRodes <rominarodes@gmail.com>
2022-06-20 21:38:52 -0300

    falta query de footer y estilo formulario

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.get('/', (req,res) => {
 app.get('/crear', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/crear-producto.html'))
 });
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
 
 
-app.listen(3001, () => console.log("servidor escuchando puerto 3001 funcionando"))
+
+//app.listen(3001, () => console.log("servidor escuchando puerto 3001 funcionando"))
+app.set('puerto',process.env.PORT || 3000)
+
+
+
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
